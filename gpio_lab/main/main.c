5f0b00857c0cb50e98493b9e598777eedde5913c
samaj43981 <97647605+samaj43981@users.noreply.github.com>
2025-10-25 20:50:08 +0700

    Step 2: ปิดไฟ LED

--- gpio_lab/main/main.c
+++ gpio_lab/main/main.c
@@ -1,7 +1,7 @@
-#include "driver/gpio.h"
-#include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
 
 #define LED_GPIO GPIO_NUM_2
 
@@ -9,10 +9,16 @@ void app_main(void) {
     // กำหนด GPIO 2 เป็น output
     gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
     
-    // เปิด LED (HIGH = 3.3V)
+    // เปิด LED ก่อน (HIGH = 3.3V)
     gpio_set_level(LED_GPIO, 1);
+    ESP_LOGI("LED", "LED is ON for 3 seconds...");
     
-    ESP_LOGI("LED", "LED is ON!");
+    // รอ 3 วินาที
+    vTaskDelay(pdMS_TO_TICKS(3000));
+    
+    // ปิด LED (LOW = 0V)
+    gpio_set_level(LED_GPIO, 0);
+    ESP_LOGI("LED", "LED is OFF!");
     
     // ป้องกันโปรแกรมจบทันที (รอ forever)
     while(1) {
